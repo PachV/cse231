3dc023a06f5f6df53e6167a203b14bed82cfac70
Pach <you@example.com>
2022-10-20 01:41:56 -0400

    added documentations

--- week7/project/proj06.py
+++ week7/project/proj06.py
@@ -1,6 +1,5 @@
 #https://www.cse.msu.edu/~cse231/Online/Projects/Project06/
     
-import csv
 from operator import itemgetter
 
 MENU = "\nWelcome to Genshin Impact Character Directory\n\
@@ -10,8 +9,6 @@ MENU = "\nWelcome to Genshin Impact Character Directory\n\
         3. Filter characters by element, weapon, and rarity\n\
         4. Quit the program\n\
         Enter option: "
-
-INVALID_INPUT = "\nInvalid input"
 
 CRITERIA_INPUT = "Choose the following criteria\n\
                  1. Element\n\
@@ -24,25 +21,34 @@ HEADER_FORMAT = "\n{:20s}{:10s}{:10s}{:<10s}{:25s}"
 ROW_FORMAT = "{:20s}{:10s}{:10s}{:<10d}{:25s}"
 
 def open_file():
-    '''Docstring'''
+    '''
+    asks user for input file, and returns the file that is requested
+    '''
     to_open = input("Enter file name: ")
     if to_open == "data_small.csv" or to_open == "data.csv":
         return open(f"{to_open}", "r")
 
     else:
         print("\nError opening file. Please try again.")
-        return open_file() # somehow this worked xdddd
+        return open_file() #somehow this works xdddd, it loops to the func
+                           # with no "data"
+                           # i figured this out while testing func loops
 
 
-def read_file(fp) -> tuple: # tuples version
-    '''Docstring'''
+def read_file(fp) -> tuple: 
+    '''
+        reads the file and reorder it and convert it into tuple
+        returns lists of toples
+    '''
     output = []
     fp.readline()
     for i in fp:
+        # the gamer way to do it 😎😎😎 
         i = i.strip().split(",")
         
         cara, rar, ele,wea,reg = i
-        if reg == "":
+        if reg == "": 
+            # reg element is the only one that potentially can be empty or ""
             reg = None
         rar = int(rar)
 
@@ -53,32 +59,42 @@ def read_file(fp) -> tuple: # tuples version
     return output
 
 def get_characters_by_criterion(list_of_tuples: list, criteria: str or int, value: str or int) -> list: # 2
-    '''Docstring'''
-    # print(list_of_tuples)
+    '''
+    matches with all the parameters that the user want,
+    and returns them into a list of lists
+    '''
     match = []
-    criteria1 = int(criteria)
+    criteria = int(criteria)
+    # this is before i knew the sort_char function, you can ignore this TO...
     for i in range(len(list_of_tuples)):
         _ ,ele,wea,rar,reg = list_of_tuples[i]
         tmp_list = [_ ,ele,wea,rar,reg]
+    # HERE
 
-        if criteria1 == 3: # if looking for rareity
+    ## idk if this func still works without the area i listed xd
+    ## and im too lazy to test it
+
+        if criteria == 3: # if looking for rareity
             value = int(value)
-            if tmp_list[criteria1] == value:
+            if tmp_list[criteria] == value:
                 match.append(tmp_list)
 
         else: # everything else
-            if tmp_list[criteria1] == None:
-                tmp_list[criteria1] = "N/A"
-            if tmp_list[criteria1].lower() == value.lower():
+            if tmp_list[criteria] == None:
+                tmp_list[criteria] = "N/A"
+            if tmp_list[criteria].lower() == value.lower():
                 match.append(tmp_list)
 
-
-    return(match)
+    return match
 
 def get_region_list(master_list) -> list:
-    '''Docstring'''
+    '''
+    find the region that isnt in the list, 
+    then sort and return the region that is seen
+    '''
     regions = []
     for i in range(len(master_list)):
+        # we only want the last element within the list of lists
         _,_,_,_,local_region = master_list[i]
         if local_region not in regions:
             if local_region == None:
@@ -90,37 +106,55 @@ def get_region_list(master_list) -> list:
     return regions
 
 def sort_characters(list_of_tuples):
-    '''Docstring'''
+    '''
+    reorder the list of tuples to the standard one
+    with rarity being on top
+
+    '''
     cleaned = []
     for i in range(len(list_of_tuples)):
+        # declare var for each element
         _ ,ele,wea,rar,reg = list_of_tuples[i]
+        # reorder the var for each element
         tmp_list = [_ ,ele,wea,rar,reg]
         cleaned.append(tmp_list)
-    sotred_clean = sorted(cleaned, key=itemgetter(0))
+
+    sotred_clean = sorted(cleaned, key=itemgetter(0)) # not fully cleaned/sort
     sorted_clean = sorted(sotred_clean, key=itemgetter(3),reverse=True)
     return sorted_clean
 
 def display_characters(list_of_tuples):
-    '''Docstring'''
+    '''
+    intakes a list that is already operated in other funcs, and reformat
+    them to make them look nice to the end user
+    '''
     print("{:21s}{:10s}{:10s}{:<10s}{:25s}".format("\nCharacter", "Element", "Weapon","Rarity", "Region"))
+    #      ^^^^^^ test case says it wanted 21... 
     for i in range(len(list_of_tuples)):
         char,ele,wea,rar,reg = list_of_tuples[i]
+
         if reg == None:
             reg = "N/A"
         print(f"{char:20s}{ele:10s}{wea:10s}{rar:<10d}{reg:25s}") 
     
 def get_option():
-    '''Docstring'''
+    '''prints out the menu and asks for the number'''
+
     print(MENU)
     optionz = input("")
     return optionz # the main function will do the matching
 
 def get_characters_by_criteria(master_list: tuple, element: str, weapon: str, rarity:int): #3
-    '''Docstring'''
+    '''
+    match with all the requirements that the user wants
+    and return them into a list
+    '''
 
     match = []
     for i in master_list:
-        # print(i)
+        # [1] is elemtn, [2] is weapon, [3] is rarity
+        # i could just do all of these in one if statement...
+
         if i[1].lower() == element.lower():
             if i[2].lower() == weapon.lower():
                 if i[3] == rarity:
@@ -131,20 +165,16 @@ def main():
     file_pointer = open_file()
     bad_list_of_tuples = read_file(file_pointer)
 
-    # print(list_of_tuples)
     list_of_tuples = sort_characters(bad_list_of_tuples)
 
     while True:
         usr_option = get_option()
-
 
         if usr_option == "1":
             print("Regions:")
             regions =get_region_list(list_of_tuples)
             print(", ".join(regions), end= "\n")
             continue
-                
-
 
         elif usr_option == "2":
             criteria = input(CRITERIA_INPUT)
@@ -153,17 +183,15 @@ def main():
             if rtrn_criteria == []:
                 print("\nNothing to print.")
                 continue
-
             else:
                 display_characters(rtrn_criteria)
                 continue
-
         
         elif usr_option == "3":
             element = input("Enter element: ")
             weapon = input("\nEnter weapon: ")
 
-            while True:
+            while True: # keeps asking for actual intergers
                 try:
                     rarity = int(input("\nEnter rarity: "))
                     break
