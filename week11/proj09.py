12f0de903e90b7ecd4c3e4f6bb078b15026e4007
Pach <pachvrotmg@gmail.com>
2022-11-11 23:04:08 -0500

    init for week11

---
+++ week11/proj09.py
@@ -0,0 +1,46 @@
+# https://www.cse.msu.edu/~cse231/Online/Projects/Project08/Project08.pdf
+import csv
+
+MENU = '''\nSelect an option from below:
+            (1) Display all companies in the New York Stock Exchange
+            (2) Display companies' symbols
+            (3) Find max price of a company
+            (4) Find the company with the maximum stock price
+            (5) Find the average price of a company's stock
+            (6) quit
+    '''
+WELCOME = "Welcome to the New York Stock Exchange.\n"
+    
+def open_file():
+    '''Docstring'''
+    pass
+
+def read_file(securities_fp):
+    '''Docstring'''
+    pass
+        
+def add_prices (master_dictionary, prices_file_pointer):
+    '''Docstring'''
+    pass
+    
+def get_max_price_of_company (master_dictionary, company_symbol):
+    '''Docstring'''
+    pass
+
+def find_max_company_price (master_dictionary):
+    '''Docstring'''
+    pass
+
+def get_avg_price_of_company (master_dictionary, company_symbol):
+    '''Docstring'''
+    pass
+            
+def display_list (lst):  # "{:^35s}"
+    '''Docstring'''
+    pass
+    
+def main():
+    pass
+       
+if __name__ == "__main__": 
+    main() 
