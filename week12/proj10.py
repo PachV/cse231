b9f7fea808ceb0317f55239266eff7faa37bb62e
Pach <pachvrotmg@gmail.com>
2022-11-19 02:14:05 -0500

    init

---
+++ week12/proj10.py
@@ -0,0 +1,156 @@
+
+from cards import Card, Deck
+
+MENU ='''Prompt the user for an option and check that the input has the 
+       form requested in the menu, printing an error message, if not.
+       Return:
+    TT s d: Move card from end of Tableau pile s to end of pile d.
+    TF s d: Move card from end of Tableau pile s to Foundation d.
+    WT d: Move card from Waste to Tableau pile d.
+    WF d: Move card from Waste to Foundation pile d.
+    SW : Move card from Stock to Waste.
+    R: Restart the game (after shuffling)
+    H: Display this menu of choices
+    Q: Quit the game        
+    '''
+def initialize():
+    '''Docstring'''
+    pass
+    
+def display(tableau, stock, foundation, waste):
+    """ display the game setup """
+    stock_top_card = "empty"
+    found_top_cards = ["empty","empty","empty","empty"]
+    waste_top_card = "empty"
+    if len(waste):
+        waste_top_card = waste[-1] 
+    if len(stock):
+        stock_top_card = "XX" #stock[-1]
+    for i in range(4):
+        if len(foundation[i]):
+            found_top_cards[i] = foundation[i][-1]
+    print()
+    print("{:5s} {:5s} \t\t\t\t\t {}".format("stock","waste","foundation"))
+    print("\t\t\t\t     ",end = '')
+    for i in range(4):
+        print(" {:5d} ".format(i+1),end = '')
+    print()
+    print("{:5s} {:5s} \t\t\t\t".format(str(stock_top_card), str(waste_top_card)), end = "")
+    for i in found_top_cards:
+        print(" {:5s} ".format(str(i)), end = "")
+    print()
+    print()
+    print()
+    print()
+    print("\t\t\t\t\t{}".format("tableau"))
+    print("\t\t ", end = '')
+    for i in range(7):
+        print(" {:5d} ".format(i+1),end = '')
+    print()
+    # calculate length of longest tableau column
+    max_length = max([len(stack) for stack in tableau])
+    for i in range(max_length):
+        print("\t\t    ",end = '')
+        for tab_list in tableau:
+            # print card if it exists, else print blank
+            try:
+                print(" {:5s} ".format(str(tab_list[i])), end = '')
+            except IndexError:
+                print(" {:5s} ".format(''), end = '')
+        print()
+    print()
+    
+
+def stock_to_waste( stock, waste ):
+    '''Docstring'''
+    pass
+    
+       
+def waste_to_tableau( waste, tableau, t_num ):
+    '''Docstring'''
+    pass
+
+def waste_to_foundation( waste, foundation, f_num ):
+    '''Docstring'''
+    pass
+
+def tableau_to_foundation( tableau, foundation, t_num, f_num ):
+    '''Docstring'''
+    pass
+
+def tableau_to_tableau( tableau, t_num1, t_num2 ):
+    '''Docstring'''
+    pass
+    
+def check_win (stock, waste, foundation, tableau):
+    '''Docstring'''
+    pass
+
+def parse_option(in_str):
+    '''Prompt the user for an option and check that the input has the 
+           form requested in the menu, printing an error message, if not.
+           Return:
+        TT s d: Move card from end of Tableau pile s to end of pile d.
+        TF s d: Move card from end of Tableau pile s to Foundation d.
+        WT d: Move card from Waste to Tableau pile d.
+        WF d: Move card from Waste to Foundation pile d.
+        SW : Move card from Stock to Waste.
+        R: Restart the game (after shuffling)
+        H: Display this menu of choices
+        Q: Quit the game        
+        '''
+    option_list = in_str.strip().split()
+    
+    opt_char = option_list[0][0].upper()
+    
+    if opt_char in 'RHQ' and len(option_list) == 1:  # correct format
+        return [opt_char]
+    
+    if opt_char == 'S' and len(option_list) == 1:
+        if option_list[0].upper() == 'SW':
+            return ['SW']
+    
+    if opt_char == 'W' and len(option_list) == 2:
+        if option_list[0].upper() == 'WT' or option_list[0].upper() == 'WF':
+            dest = option_list[1] 
+            if dest.isdigit():
+                dest = int(dest)
+                if option_list[0].upper() == 'WT' and (dest < 1 or dest > 7):
+                    print("\nError in Destination")
+                    return None
+                if option_list[0].upper() == 'WF' and (dest < 1 or dest > 4):
+                    print("\nError in Destination")
+                    return None
+                opt_str = option_list[0].strip().upper()
+                return [opt_str,dest]
+                               
+    if opt_char == 'T' and len(option_list) == 3 and option_list[1].isdigit() \
+        and option_list[2].isdigit():
+        opt_str = option_list[0].strip().upper()
+        if opt_str in ['TT','TF']:
+            source = int(option_list[1])
+            dest = int(option_list[2])
+            # check for valid source values
+            if opt_str in ['TT','TF'] and (source < 1 or source > 7):
+                print("\nError in Source.")
+                return None
+            #elif opt_str == 'MFT' and (source < 0 or source > 3):
+                #print("Error in Source.")
+                #return None
+            # source values are valid
+            # check for valid destination values
+            if (opt_str =='TT' and (dest < 1 or dest > 7)) \
+                or (opt_str == 'TF' and (dest < 1 or dest > 4)):
+                print("\nError in Destination")
+                return None
+            return [opt_str,source,dest]
+
+    print("\nError in option:", in_str)
+    return None   # none of the above
+
+
+def main():   
+    pass
+
+if __name__ == '__main__':
+     main()
