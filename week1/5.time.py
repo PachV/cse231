819292e2eb16a4af0e423583b5d73fec3eb8e70a
Pach <pachvrotmg@gmail.com>
2022-11-20 04:18:38 -0500

    added IO files and pdfs

--- week1/5.time.py
+++
@@ -1,6 +0,0 @@
-# secs_str = input("Input seconds: \n") # do not change this line
-secs_int = 80000
-hours = secs_int // 3600
-minutes = (secs_int - hours * 3600) // 60
-seconds = secs_int - (hours * 3600) - (minutes * 60)
-print(hours,":",minutes,":",seconds) # do not change this line
